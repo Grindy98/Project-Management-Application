d71b70201b8b05853370dca0fc18c4b0886d7d65
Stefan Malanik <stefanmalanik@gmail.com>
2021-04-27 00:37:24 +0300

    Actually added module-info.java this time

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,8 @@
-module $MODULE_NAME$ {
+module Project.Management.Application {
+    requires javafx.controls;
+    requires javafx.fxml;
+    opens scene.controller.implementations to javafx.fxml;
+    exports scene;
+    exports scene.controller;
+    exports scene.controller.implementations;
 }
