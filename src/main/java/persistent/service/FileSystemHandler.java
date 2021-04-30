21d757df67c5dd2903bc5f63a62aacf0e8478d0b
Stefan Malanik <stefanmalanik@gmail.com>
2021-04-30 14:15:14 +0300

    PROJ-7 Working prototype for save

--- src/main/java/persistent/service/FileSystemHandler.java
+++ src/main/java/persistent/service/FileSystemHandler.java
@@ -1,16 +1,54 @@
 package persistent.service;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileSystemHandler {
+
+    public enum FileType{
+        TEST("/save_models/empty_save.json",
+                "testPersistent");
+        //PROJECT,
+        //USER;
+
+        private final Path savePath;
+
+        FileType(String emptyLocation, String saveFileName){
+            savePath = FileSystemHandler.getPathToFile("config", saveFileName);
+            if(!Files.exists(savePath)){
+                // Copy file from path
+                try {
+                    URL emptyURL = this.getClass().getResource(emptyLocation);
+                    if(emptyURL == null){
+                        throw new IllegalArgumentException("Empty file not found!");
+                    }
+                    FileUtils.copyFile(new File(emptyURL.toURI()),
+                            savePath.toFile());
+                } catch (IOException | URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+
+        public Path getSavePath(){
+            return savePath;
+        }
+    }
 
     private static final String APPLICATION_FOLDER = ".project-management-app";
     private static final String USER_FOLDER = System.getProperty("user.home");
 
     public static final Path APP_FULL_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
-    public static Path getPathToFile(String... path) {
+    static Path getPathToFile(String... path) {
         return APP_FULL_PATH.resolve(Paths.get(".", path));
     }
 }
--- src/main/java/scene/MainApp.java
+++ src/main/java/scene/MainApp.java
@@ -1,16 +1,15 @@
 package scene;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import persistent.TestPersistent;
 import persistent.TestPersistentSubclass;
-import persistent.service.ListSave;
 import scene.controller.implementations.StartPageController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainApp extends Application{
 
@@ -23,13 +22,18 @@ public class MainApp extends Application{
         stage.show();
 
         TestPersistent t = new TestPersistent(5, "me");
-        TestPersistent t2 = new TestPersistentSubclass(5, "me2", "fi21f9bwq");
+        TestPersistent t2 = new TestPersistent(7, "me3");
+        TestPersistent t3 = new TestPersistentSubclass(5, "me2", "fi21f9bwq");
 
-        ListSave<TestPersistent> list = new ListSave<>(TestPersistent.class, "testPersistent",
-                "/save_models/empty_save.json");
-        list.add(t);
-        list.add(t2);
-        list.save();
+        ArrayList<TestPersistent> arr = TestPersistent.load();
+        System.out.println(arr);
+        arr = new ArrayList<>(3);
+        arr.add(t);
+        arr.add(t2);
+        arr.add(t3);
+        System.out.println(arr);
+
+        TestPersistent.save(arr);
     }
 
     public static void main(String[] args) {
