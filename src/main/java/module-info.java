0e9c1ac599b058926d6930c85dd23a77908f7524
Stefan Malanik <stefanmalanik@gmail.com>
2021-05-02 19:53:05 +0300

    PROJ-10 Added TableView structure to select team members to be added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,13 @@ module Project.Management.Application {
     requires javafx.controls;
     requires javafx.fxml;
     opens scene.controller.implementations to javafx.fxml;
+    opens scene.controller.implementations.popups to javafx.fxml;
     opens scene.list.elements to javafx.fxml;
 
     exports scene;
     exports scene.controller;
     exports scene.list;
     exports scene.controller.implementations;
+    exports scene.controller.implementations.popups;
     exports scene.list.elements;
 }
