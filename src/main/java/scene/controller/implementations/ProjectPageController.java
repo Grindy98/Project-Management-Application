e5f34cc3034555f7a6976dface4545ef54b383c6
Claudiu <negruclaudiu389@yahoo.com>
2021-05-11 18:50:25 +0300

    PROJ-9 basic project page

---
+++ src/main/java/scene/controller/implementations/ProjectPageController.java
@@ -0,0 +1,10 @@
+package scene.controller.implementations;
+
+import scene.controller.SceneController;
+
+public class ProjectPageController extends SceneController {
+
+    public ProjectPageController(){
+        super("/pages/project_page.fxml", 500, 500);
+    }
+}
