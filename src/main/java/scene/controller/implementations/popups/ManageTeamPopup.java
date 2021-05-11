222819af3cdf306eacbf56b79b83108325df2426
Claudiu <negruclaudiu389@yahoo.com>
2021-05-11 18:57:12 +0300

    PROJ-9 manage team popup added

---
+++ src/main/java/scene/controller/implementations/popups/ManageTeamPopup.java
@@ -0,0 +1,10 @@
+package scene.controller.implementations.popups;
+
+import scene.controller.SceneController;
+
+public class ManageTeamPopup extends SceneController {
+
+    public ManageTeamPopup(){
+        super("/pages/popups/manage_team_popup.fxml", 400, 400);
+    }
+}
