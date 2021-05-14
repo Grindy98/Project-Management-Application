45c8933c145c772e70df63efebbc36f2089e1745
Stefan Malanik <stefanmalanik@gmail.com>
2021-05-14 16:33:22 +0300

    PROJ-26 Added full user save test AND found a bug with it
    BUG FIX: When the User.load method was called, the previously stored users did not get removed

--- src/test/java/persistent/user/UserSaveTest.java
+++ src/test/java/persistent/user/UserSaveTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import persistent.service.FileSystemHandler;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
@@ -20,6 +19,19 @@ class UserSaveTest {
     private static final String emptyLocation = "/save_models/empty_save.json";
     private static final String testSave = "/user/test_save.json";
 
+
+    private void printToCons(File f){
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(f));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     private void copyToSaveFile(String from){
         // Copy file from path
         try {
@@ -27,8 +39,12 @@ class UserSaveTest {
             if(emptyURL == null){
                 throw new IllegalArgumentException("Empty file not found!");
             }
+            System.out.println("before");
+            printToCons(FileSystemHandler.FileType.USER.getSavePath().toFile());
             FileUtils.copyFile(new File(emptyURL.toURI()),
                     FileSystemHandler.FileType.USER.getSavePath().toFile());
+            System.out.println("after");
+            printToCons(FileSystemHandler.FileType.USER.getSavePath().toFile());
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
@@ -62,6 +78,29 @@ class UserSaveTest {
         // File increased in size
         assertTrue(FileUtils.sizeOf(new File(UserSaveTest.class.getResource(emptyLocation).getFile())) <
                 FileUtils.sizeOf(FileSystemHandler.FileType.USER.getSavePath().toFile()));
+    }
+
+    @Test
+    void saveThenLoad(){
+        copyToSaveFile(emptyLocation);
+        User.load();
+        System.out.println(User.getUsers());
+        User u = new TeamMember("test_user", "12345678", "9 Maple Drive", "0123798213");
+        User.getUsers().put(u.getUsername(), u);
+        User.save();
+        // File increased in size
+        assertTrue(FileUtils.sizeOf(new File(UserSaveTest.class.getResource(emptyLocation).getFile())) <
+                FileUtils.sizeOf(FileSystemHandler.FileType.USER.getSavePath().toFile()));
+        User.load();
+        List<User> list = List.copyOf(User.getUsers().values());
+        assertEquals(1, list.size());
+        // One proj loaded
+        u = list.get(0);
+        assertEquals("test_user", u.getUsername());
+        assertTrue(u.checkPassword("12345678"));
+        assertEquals("9 Maple Drive", u.getAddress());
+        assertEquals("0123798213", u.getPhone());
+        assertTrue(u instanceof TeamMember);
     }
 
     @AfterAll
