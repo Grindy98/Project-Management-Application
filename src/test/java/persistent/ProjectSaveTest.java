330641e985d33214c0b820386973a64e092533da
Stefan Malanik <stefanmalanik@gmail.com>
2021-05-14 16:46:46 +0300

    PROJ-26 Added full project save test and fixed same bug

--- src/test/java/persistent/ProjectSaveTest.java
+++ src/test/java/persistent/ProjectSaveTest.java
@@ -8,6 +8,7 @@ import persistent.service.FileSystemHandler;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -65,6 +66,34 @@ class ProjectSaveTest {
         // File increased in size
         assertTrue(FileUtils.sizeOf(new File(ProjectSaveTest.class.getResource(emptyLocation).getFile())) <
                 FileUtils.sizeOf(FileType.PROJECT.getSavePath().toFile()));
+    }
+
+    @Test
+    void saveThenLoad(){
+        copyToSaveFile(emptyLocation);
+        Project.load();
+        Project p = null;
+        try {
+            p = new Project(List.of("user1"), "user2", "name", "desc");
+        } catch (ProjectValidationFailedException e) {
+            fail("No exception should be thrown");
+        }
+        Project.getProjects().add(p);
+        Project.save();
+        // File increased in size
+        assertTrue(FileUtils.sizeOf(new File(ProjectSaveTest.class.getResource(emptyLocation).getFile())) <
+                FileUtils.sizeOf(FileType.PROJECT.getSavePath().toFile()));
+        Project.load();
+        List<Project> list = Project.getProjects();
+        assertEquals(1, list.size());
+        // One proj loaded
+        Project read_p = list.get(0);
+
+        assertTrue(read_p.getMemberUsernameList().containsAll(p.getMemberUsernameList()) &&
+                p.getMemberUsernameList().containsAll(read_p.getMemberUsernameList()));
+        assertEquals(p.getOwnerUsername(), read_p.getOwnerUsername());
+        assertEquals(p.getName(), read_p.getName());
+        assertEquals(p.getDescription(), read_p.getDescription());
     }
 
     @AfterAll
